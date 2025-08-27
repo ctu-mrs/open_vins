de9448f33e3b772c48d733ffbe1eafcc83c78909
Jakub Lenner <lennejak@fel.cvut.cz>
2025-08-27 15:44:30 +0200

    forgot to add the launchfile for the composable msckf node from OpenVINS

---
+++ ov_msckf/launch/subscribe_composable.launch.py
@@ -0,0 +1,160 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, LogInfo, OpaqueFunction
+from launch.conditions import IfCondition, UnlessCondition
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch_ros.actions import ComposableNodeContainer, Node, LoadComposableNodes
+from launch_ros.descriptions import ComposableNode
+from ament_index_python.packages import get_package_share_directory, get_package_prefix
+import os
+import sys
+
+launch_args = [
+    DeclareLaunchArgument(
+        name='uav_name',
+        default_value=os.environ["UAV_NAME"]
+    ),
+    DeclareLaunchArgument(
+        name="rviz_enable", default_value="false", description="enable rviz node"
+    ),
+    DeclareLaunchArgument(
+        name="config",
+        default_value="euroc_mav",
+        description="euroc_mav, tum_vi, rpng_aruco...",
+    ),
+    DeclareLaunchArgument(
+        name="config_path",
+        default_value="",
+        description="path to estimator_config.yaml. If not given, determined based on provided 'config' above",
+    ),
+    DeclareLaunchArgument(
+        name="verbosity",
+        default_value="INFO",
+        description="ALL, DEBUG, INFO, WARNING, ERROR, SILENT",
+    ),
+    DeclareLaunchArgument(
+        name="use_stereo",
+        default_value="true",
+        description="if we have more than 1 camera, if we should try to track stereo constraints between pairs",
+    ),
+    DeclareLaunchArgument(
+        name="max_cameras",
+        default_value="2",
+        description="how many cameras we have 1 = mono, 2 = stereo, >2 = binocular (all mono tracking)",
+    ),
+    DeclareLaunchArgument(
+        name="save_total_state",
+        default_value="false",
+        description="record the total state with calibration and features to a txt file",
+    ),
+    DeclareLaunchArgument(
+        name="use_intra_process",
+        default_value="false",
+        description="enable ROS 2 intra-process communication for zero-copy message passing",
+    ),
+    DeclareLaunchArgument(
+        name="container_name",
+        default_value="openvins_container",
+        description="name of the component container",
+    ),
+    DeclareLaunchArgument(
+        name="standalone",
+        default_value="true",
+        description="determines whether this node will be running in it's own component container or in the one who's name is provided"
+    )
+]
+
+def launch_setup(context):
+    config_path = LaunchConfiguration("config_path").perform(context)
+    if config_path == '':
+        #configs_dir = os.path.join(get_package_share_directory("ov_msckf"), "config")
+        #available_configs = os.listdir(configs_dir)
+        config = LaunchConfiguration("config").perform(context)
+        # if config in available_configs:
+        #     config_path = os.path.join(
+        #                     get_package_share_directory("ov_msckf"),
+        #                     "config",config,"estimator_config.yaml"
+        #                 )
+        config_path = os.path.join(
+            get_package_share_directory("ov_msckf"),
+            "config",config,"estimator_config.yaml"
+        )
+    
+        # else:
+        #     return [
+        #         LogInfo(
+        #             msg="ERROR: unknown config: '{}' - Available configs are: {} - not starting OpenVINS".format(
+        #                 config, ", ".join(available_configs)
+        #             )
+        #         )
+        #     ]
+    else:
+        if not os.path.isfile(config_path):
+            return [
+                LogInfo(
+                    msg="ERROR: config_path file: '{}' - does not exist. - not starting OpenVINS".format(
+                        config_path)
+                    )
+            ]
+            
+    print("config_path: ", config_path)
+    
+    # Create the composable node description
+    msckf_node = ComposableNode(
+        package="ov_msckf",
+        plugin="msckf_component::SubscribeMSCKF",  # Update this with the actual component class name
+        name="run_subscribe_msckf",
+        namespace = LaunchConfiguration('uav_name'),
+        parameters=[
+            {"verbosity": LaunchConfiguration("verbosity")},
+            {"use_stereo": LaunchConfiguration("use_stereo")},
+            {"max_cameras": LaunchConfiguration("max_cameras")},
+            {"save_total_state": LaunchConfiguration("save_total_state")},
+            {"config_path": config_path},
+        ],
+        extra_arguments=[{"use_intra_process_comms": LaunchConfiguration("use_intra_process")}],
+        remappings=[("/imu_raw", PathJoinSubstitution(["/", LaunchConfiguration('uav_name'), 'imu_filtered'])),
+                    ("/image_raw", PathJoinSubstitution(["/", LaunchConfiguration('uav_name'), 'image_raw']))]
+    )
+    
+    loader = LoadComposableNodes(
+        condition=UnlessCondition(LaunchConfiguration('standalone')),
+        composable_node_descriptions=[msckf_node],
+        target_container=LaunchConfiguration('container_id'),
+    )
+    
+    # Create the component container
+    container = ComposableNodeContainer(
+        name=LaunchConfiguration("container_name"),
+        namespace=LaunchConfiguration('uav_name'),
+        package="rclcpp_components",
+        executable="component_container",
+        condition=IfCondition(LaunchConfiguration("standalone")),
+        composable_node_descriptions=[msckf_node],
+        output='screen',
+        arguments=['--ros-args', '--log-level', 'info'],
+    )
+
+    # RViz node (remains as regular node)
+    rviz_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        condition=IfCondition(LaunchConfiguration("rviz_enable")),
+        arguments=[
+            "-d"
+            + os.path.join(
+                get_package_share_directory("ov_msckf"), "launch", "display_ros2.rviz"
+            ),
+            "--ros-args",
+            "--log-level",
+            "warn",
+        ],
+    )
+
+    return [loader, container, rviz_node]
+
+
+def generate_launch_description():
+    opfunc = OpaqueFunction(function=launch_setup)
+    ld = LaunchDescription(launch_args)
+    ld.add_action(opfunc)
+    return ld
